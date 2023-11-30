66cf7d91afe9241400e4561593fe9de9b474977d
John P Varghese <github@john00.anonaddy.com>
2023-11-30 09:40:55 +0530

    Cnditional Rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -8,30 +8,35 @@ import Conversation from './components/Conversation';
 import ChatSectionPlaceholder from './components/ChatSectionPlaceholder';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const App = () => {
   const [chatPlaceHolder, setChatPlaceHolder] = useState(true);
   const [selectedChat, setSelectedChat] = useState();
+  const [userLogged, setUserLogged] = useState(false);
 
   const HomePage =()=>{
-    <div className='app'>
-        <ContactList setChatPlaceHolder={setChatPlaceHolder} setSelectedChat={setSelectedChat} />
-        {chatPlaceHolder ? (<ChatSectionPlaceholder />) : (<Conversation selectedChat={selectedChat} />)}
-        {/* <Conversation /> */}
-    </div>
+    if(userLogged){
+      return(
+        <div className='app'>
+          <ContactList setChatPlaceHolder={setChatPlaceHolder} setSelectedChat={setSelectedChat} />
+            {chatPlaceHolder ? (<ChatSectionPlaceholder />) : (<Conversation selectedChat={selectedChat} />)}
+            {/* <Conversation /> */}
+        </div>
+      )
+    }else {
+      return <Login/>
+    }
   }
   
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={<HomePage/>} />
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
       <ToastContainer />
     </>
-
   )
 }
 

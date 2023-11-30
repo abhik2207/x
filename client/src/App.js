ebb7c8f990b04826e4c4149aa8e09e3e47138c5f
johnpvarghese <github@john00.anonaddy.com>
2023-11-30 10:01:40 +0530

    rendering

--- client/src/App.js
+++ client/src/App.js
@@ -13,20 +13,30 @@ import { Route, Routes } from 'react-router-dom';
 const App = () => {
   const [ chatPlaceHolder, setChatPlaceHolder ] = useState(true);
   const [ selectedChat, setSelectedChat ] = useState();
+  const [userLogged, setUserLogged] = useState(false);
 
-  
+  if(localStorage.getItem('convoverseUserLoginId')){
+    setUserLogged(true);
+  }
+  const HomePage =()=>{
+    if(userLogged){
+      return(
+        <div className='app'>
+          <ContactList setChatPlaceHolder={setChatPlaceHolder} setSelectedChat={setSelectedChat} />
+            {chatPlaceHolder ? (<ChatSectionPlaceholder />) : (<Conversation selectedChat={selectedChat} />)}
+            {/* <Conversation /> */}
+        </div>
+      )
+    }else {
+      return <Login/>
+    }
+  }
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Login />} />
         <Route path='/signup' element={<Signup />} />
-        <Route path='/home' element={
-          <div className='app'>
-            <ContactList setChatPlaceHolder={setChatPlaceHolder} setSelectedChat={setSelectedChat} />
-            {chatPlaceHolder ? (<ChatSectionPlaceholder />) : (<Conversation selectedChat={selectedChat} />)}
-            {/* <Conversation /> */}
-          </div>} />
+        <Route path='/home' element={<HomePage/>} />
       </Routes>
       <ToastContainer />
     </>
